c481c65b359d65770ceb2a14a1cd4b230d5d9c7e
Ram-eez <rameez2838@gmail.com>
2025-01-24 20:44:24 +0530

    Image to byte and total no of images

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,5 +1,29 @@
 package cmd
 
-func UDPListen() {
+import (
+	"fmt"
+	"log"
+	"net"
 
+	"main.go/internal"
+)
+
+func UDPListen() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ln, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ln.Close()
+
+	fmt.Println("Running on port :3000")
+
+	for i := 0; i <= internal.GetTotalImages("/home/rameez/Downloads/frametest/"); i++ {
+
+	}
 }
--- internal/capture.go
+++ internal/capture.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"log"
+	"os"
 
 	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 )
@@ -10,10 +11,32 @@ import (
 func FFmpegFrameCapture() {
 	err := ffmpeg_go.Input("/home/rameez/Downloads/New Folder/DespicableMe/DespicableMe.mp4").
 		Output("/home/rameez/Downloads/frametest/frame_%02d.jpg", ffmpeg_go.KwArgs{"vf": "fps=1", "q:v": "1"}).
+		// add these lines in ffmpeg_go.KwArgs{} if you want to encode the file into H.264 codex
+		// "c:v": "libx264"
+		// "preset": "fast",      // Encoding speed (fast, medium, slow)
 		Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Frames Extracted Successfully")
+}
+
+func ImageToByte(frame string) ([]byte, error) {
+	byteStream, err := os.ReadFile(frame)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return byteStream, nil
+}
+
+func GetTotalImages(path string) int {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return len(files)
 }
