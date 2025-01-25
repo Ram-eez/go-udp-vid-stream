5b206e3f5747ac6718c22c8cd623620f7c413eca
Ram-eez <rameez2838@gmail.com>
2025-01-25 19:11:50 +0530

    UDP Dial func

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"log"
+	"net"
+)
+
+func UDPDial() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ln, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ln.Close()
+
+}
